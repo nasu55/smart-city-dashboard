ea6758abcae517fd9512b07a241292f5e910a66f
nasu55 <timannan93@gmail.com>
2025-01-31 16:04:31 +0530

    qwer456

--- src/components/Tables/shop.tsx
+++ src/components/Tables/shop.tsx
@@ -58,39 +58,7 @@ const ShopTable = ({ listOfShops }: Props) => {
             <div className="ml-7 flex justify-between">
               <div>
                 <form className="mx-auto max-w-md">
-                <label
-                    htmlFor="default-search"
-                    className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Search
-                  </label>
-                  <div className="relative">
-                    <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
-                      <svg
-                        className="h-4 w-4 text-gray-500 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                        />
-                      </svg>
-                    </div>
-                    {/* <input
-                      type="search"
-                      id="default-search"
-                      className="block w-full rounded-lg border  border-gray-300 bg-gray-50 p-1 px-20 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                      placeholder="Search Shop"
-                      value={searchTerm}
-                      onChange={(e) => setSearchTerm(e.target.value)}
-                    /> */}
-                  </div>
+
                 </form>
               </div>
 
